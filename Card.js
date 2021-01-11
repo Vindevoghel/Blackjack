d7a6fe01922e5f4a7f40233374e5efb8d7ae5886
vindevoghel <yvindevoghel@gmail.com>
2021-01-11 19:34:40 +0100

    Card class and Deck class created, shuffle working as well

---
+++ Card.js
@@ -0,0 +1,10 @@
+//Creates Card Object which holds all the necessary information for game logic and display purposes
+class Card {
+    constructor(suit, rank, value) {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+        this.name = rank + " of " + suit;
+        this.image = "img/" + suit + rank + ".png";
+    }
+}
